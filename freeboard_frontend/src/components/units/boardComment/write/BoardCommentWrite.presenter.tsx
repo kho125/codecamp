5ac979dd71e32cdf2bed7951c94a66634c3e3be9
김경호 <kimkyeongho@gimgyeonghoui-MacBookPro.local>
2021-07-16 10:04:20 +0900

    comments

--- freeboard_frontend/src/components/units/boardComment/write/BoardCommentWrite.presenter.tsx
+++ freeboard_frontend/src/components/units/boardComment/write/BoardCommentWrite.presenter.tsx
@@ -3,9 +3,22 @@ import {
   Bar,
   Comment,
   CommentImg,
-  Comment_p,
+  Comment__p,
   CommentBox,
-  CommentInput
+  Box,
+  Comment__Bar,
+  Box__Bottom,
+  Letter,
+  CommentInput,
+  User__Box,
+  User__Img,
+  User__m,
+  User__Name,
+  User__p,
+  User__Day,
+  Comment__Img,
+  Write__Img,
+  Delete__Img
 } from "./BoardCommentWrite.styles"
 
 export default function BoardCommentWriteUI() {
@@ -14,10 +27,30 @@ export default function BoardCommentWriteUI() {
       <Wrapper>
         <Bar></Bar>
         <Comment>
-          <CommentImg src= "image/comment.png"></CommentImg>
-          <Comment_p>댓글</Comment_p>
+          <CommentImg src= "/comment.png"></CommentImg>
+          <Comment__p>댓글</Comment__p>
         </Comment>
-        <CommentBox placeholder= "개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."></CommentBox>
+        <Box>
+          <CommentBox placeholder= "개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."></CommentBox>
+          <Comment__Bar></Comment__Bar>
+            <Box__Bottom>
+              <Letter>0/100</Letter>
+              <CommentInput>등록하기</CommentInput>
+            </Box__Bottom>
+        </Box>
+
+        <User__Box>
+          <User__Img src= "/user.png"></User__Img>
+          <User__m>
+            <User__Name>노원두</User__Name>
+            <User__p>진짜 유익하고 정말 필요한 정보인 것 같아요~! 앞으로도 좋은 정보 부탁드립니다~!</User__p>
+            <User__Day>2021.02.22</User__Day>
+          </User__m>
+          <Comment__Img>
+            <Write__Img src= "/pencil.png"></Write__Img>
+            <Delete__Img src= "/delete.png"></Delete__Img>
+          </Comment__Img>
+        </User__Box>
       </Wrapper>
     </>
   )
--- freeboard_frontend/src/components/units/boardComment/write/BoardCommentWrite.styles.ts
+++ freeboard_frontend/src/components/units/boardComment/write/BoardCommentWrite.styles.ts
@@ -18,7 +18,7 @@ export const CommentImg = styled.img`
   /* background-image: url('image/comment.png'); */
 `
 
-export const Comment_p = styled.div`
+export const Comment__p = styled.div`
   font-weight: 500;
   font-size: 18px;
   line-height: 27px;
@@ -28,7 +28,85 @@ export const Comment_p = styled.div`
 
 export const CommentBox = styled.div`
   width: 100%;
-  height: 108px;
+  height: 161px;
   border: 1px solid #BDBDBD;
 `
 
+export const Box = styled.div`
+`
+
+export const Letter = styled.div`
+  margin: 14px 0 14px 20px;
+  color: #BDBDBD;
+`
+
+export const Comment__Bar = styled.div`
+  /* position: absolute; */
+  border: 1px solid #F2F2F2;
+  margin-top: -52px;
+`
+
+export const Box__Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const CommentInput = styled.button`
+  width: 91px;
+  height: 49px;
+  font-weight: 500;
+  font-size: 16px;
+  /* line-height: 24px; */
+  background-color: #000;
+  color: #fff;
+  border: none;
+`
+
+export const User__Box = styled.div`
+  width: 100%;
+  height: 130px;
+  margin-top: 20px;
+`
+
+export const User__Img = styled.img`
+  /* display: flex; */
+  margin-top: 46px;
+  margin-left: 16px;
+`
+
+export const User__m = styled.div`
+  
+`
+
+export const User__Name = styled.div`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  margin-bottom: 4px;
+`
+
+export const User__p = styled.div`
+  font-size: 16px;
+  line-height: 24px;
+  color: #4F4F4F;
+  margin-bottom: 20px;
+`
+
+export const User__Day = styled.div`
+  font-size: 12px;
+  line-height: 18px;
+  color: #BDBDBD;
+  margin-bottom: 20px;
+`
+
+export const Comment__Img = styled.div`
+
+`
+
+export const Write__Img = styled.img`
+
+`
+
+export const Delete__Img = styled.img`
+
+`
