7217b6ed04c8589f7a5b07c7a4a495d5577449b6
김경호 <kimkyeongho@gimgyeonghoui-MacBookPro.local>
2021-07-22 08:20:44 +0900

    디자인

---
+++ freeboard_frontend/src/components/layout/banner/LayoutBanner.styles.ts
@@ -0,0 +1,72 @@
+import styled from "@emotion/styled";
+
+export const Banner = styled.div`
+  height: 400px;
+  background-color: #999;
+`;
+
+export const AAA = styled.div`
+  background-image: url("/aaa/img-01.png");
+  h3 {
+    margin-top: 102px;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 71px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #fff;
+  }
+  p {
+    padding-bottom: 138px;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 18px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #fff;
+  }
+`;
+
+export const BBB = styled.div`
+  background-image: url("/aaa/img-02.png");
+  h3 {
+    margin-top: 102px;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 71px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #fff;
+  }
+  p {
+    padding-bottom: 138px;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 18px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #fff;
+  }
+`;
+
+export const CCC = styled.div`
+  background-image: url("/aaa/img-03.png");
+  h3 {
+    margin-top: 102px;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 71px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #fff;
+  }
+  p {
+    padding-bottom: 138px;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 18px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #fff;
+  }
+`;
