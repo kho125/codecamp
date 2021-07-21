4a141fcc57afe737139fc38908c210be619806c3
김경호 <kimkyeongho@gimgyeonghoui-MacBookPro.local>
2021-07-21 16:17:24 +0900

    ll

--- freeboard_frontend/pages/_app.tsx
+++ freeboard_frontend/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import "../styles/globals.css";
 import "antd/dist/antd.css";
 
+//
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
     uri: "http://backend02.codebootcamp.co.kr/graphql",
