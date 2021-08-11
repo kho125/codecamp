0a1918b2f3893c118265e507fb1a4898538ddae1
김경호 <kyeongho94@gmail.com>
2021-08-11 22:36:34 +0900

    MainPage기능

--- freeboard_frontend/src/components/units/product/main/ProductMain.queries.ts
+++ freeboard_frontend/src/components/units/product/main/ProductMain.queries.ts
@@ -1,5 +1,22 @@
 import { gql } from "@apollo/client";
 
+export const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int, $search: String) {
+    fetchBoards(page: $page, search: $search) {
+      _id
+      writer
+      title
+      createdAt
+    }
+  }
+`;
+
+export const FETCH_BOARDS_COUNT = gql`
+  query fetchBoardsCount {
+    fetchBoardsCount
+  }
+`;
+
 // export const FETCH_BOARD = gql`
 //   query fetchBoard($boardId: ID!) {
 //     fetchBoard(boardId: $boardId) {
--- freeboard_frontend/src/components/units/product/main/ProductMain.types.ts
+++ freeboard_frontend/src/components/units/product/main/ProductMain.types.ts
@@ -1,3 +1,15 @@
+import { ApolloQueryResult, OperationVariables } from "@apollo/client";
+import { SetStateAction } from "react";
+import { IQuery } from "../../../../commons/types/generated/types";
+
 export interface IProductMainUIProps {
   data: any;
+  refetch: (
+    variables?: Partial<OperationVariables> | undefined
+  ) => Promise<ApolloQueryResult<Pick<IQuery, "fetchBoards">>>;
+  dataBoardsCount?: Pick<IQuery, "fetchBoardsCount">;
+  startPage: number;
+  setStartPage: (value: SetStateAction<number>) => void;
+  onClickMoveToProductNew: () => void;
+  onClickMoveToProductDetail: () => void;
 }
