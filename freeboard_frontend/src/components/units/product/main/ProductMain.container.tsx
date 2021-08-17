ef5f5dfd8d094002a31299040e50776ca74bb0aa
김경호 <kyeongho94@gmail.com>
2021-08-17 17:25:28 +0900

    등록

--- freeboard_frontend/src/components/units/product/main/ProductMain.container.tsx
+++ freeboard_frontend/src/components/units/product/main/ProductMain.container.tsx
@@ -1,13 +1,161 @@
+// import MarketListUI from "./ProductList.presenter";
+// import { useRouter } from "next/router";
+// import { useMutation, useQuery } from "@apollo/client";
+// import { useState, useEffect } from "react";
+// import {
+//   FETCH_USER_LOGGED_IN,
+//   FETCH_USED_ITEMS,
+//   TOGGLE_USED_ITEM_PICK,
+//   FETCH_USED_ITEM_OF_THE_BEST,
+// } from "./ProductList.queries";
+// export default function MarketList() {
+//   const router = useRouter();
+
+//   const { data } =
+//     useQuery<Pick<IQuery, "fetchUserLoggedIn">>(FETCH_USER_LOGGED_IN);
+//   const { data: item, refetch } = useQuery(FETCH_USED_ITEMS);
+//   const { data: best } = useQuery(FETCH_USED_ITEM_OF_THE_BEST);
+//   const [toggleitem] = useMutation(TOGGLE_USED_ITEM_PICK);
+//   const [baskets, setBaskets] = useState([]);
+//   // useEffect(() => {
+//   //   if (!accessToken) {
+//   //     alert("로그인 해주세요!");
+//   //     router.push("/market/login");
+//   //   }
+//   // }, []);
+//   useEffect(() => {
+//     const items = JSON.parse(localStorage.getItem("baskets") || "[]");
+//     setBaskets(items);
+//     console.log(items);
+//   }, []);
+
+//   const onClickBasket = (basketData) => (event) => {
+//     console.log(basketData);
+
+//     const baskets = JSON.parse(localStorage.getItem("baskets") || "[]");
+//     console.log(baskets);
+//     let isExists = false;
+//     baskets.forEach((data) => {
+//       if (data._id === basketData._id) isExists = true;
+//     });
+//     if (isExists) {
+//       router.push(`/market/detail/${event.target.id}`);
+//       return;
+//     }
+
+//     baskets.push(basketData);
+//     localStorage.setItem("baskets", JSON.stringify(baskets));
+//     router.push(`/market/detail/${event.target.id}`);
+//   };
+
+//   const aaa = () => {
+//     router.push("/market/new");
+//   };
+
+//   const detail = (event) => {
+//     router.push(`/market/detail/${event.target.id}`);
+//   };
+
+//   const toggle = async (event) => {
+//     await toggleitem({
+//       variables: { useditemId: event.target.id },
+//       refetchQueries: [
+//         {
+//           query: FETCH_USED_ITEMS,
+//         },
+//       ],
+//     });
+//   };
+//   console.log(best);
+//   console.log(best?.fetchUseditemsOfTheBest[0].images[0]);
+//   return (
+//     <MarketListUI
+//       data={data}
+//       item={item}
+//       best={best}
+//       baskets={baskets}
+//       onClickBasket={onClickBasket}
+//       aaa={aaa}
+//       detail={detail}
+//       toggle={toggle}
+//     />
+//   );
+// }
+
+import {
+  FETCH_USER_LOGGED_IN,
+  FETCH_USED_ITEMS,
+  TOGGLE_USED_ITEM_PICK,
+  FETCH_USED_ITEM_OF_THE_BEST,
+} from "./ProductMain.queries";
 import ProductMainUI from "./ProductMain.presenter";
-import { useQuery } from "@apollo/client";
-import { FETCH_USED_ITEMS } from "./ProductMain.queries";
+import { useMutation, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
-import { MouseEvent, useState } from "react";
+import { MouseEvent, useState, useEffect } from "react";
 
 export default function ProductMain() {
+  const { data } =
+    useQuery<Pick<IQuery, "fetchUserLoggedIn">>(FETCH_USER_LOGGED_IN);
+  const { data: item } = useQuery(FETCH_USED_ITEMS);
+  const { data: best } = useQuery(FETCH_USED_ITEM_OF_THE_BEST);
+  const [toggleitem] = useMutation(TOGGLE_USED_ITEM_PICK);
+  const [baskets, setBaskets] = useState([]);
+  // useEffect(() => {
+  //   if (!accessToken) {
+  //     alert("로그인 해주세요!");
+  //     router.push("/market/login");
+  //   }
+  // }, []);
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("baskets") || "[]");
+    setBaskets(items);
+    console.log(items);
+  }, []);
+
+  const onClickBasket = (basketData) => (event) => {
+    console.log(basketData);
+
+    const baskets = JSON.parse(localStorage.getItem("baskets") || "[]");
+    console.log(baskets);
+    let isExists = false;
+    baskets.forEach((data) => {
+      if (data._id === basketData._id) isExists = true;
+    });
+    if (isExists) {
+      router.push(`/Product/detail/${event.target.id}`);
+      return;
+    }
+
+    baskets.push(basketData);
+    localStorage.setItem("baskets", JSON.stringify(baskets));
+    router.push(`/Product/detail/${event.target.id}`);
+  };
+
+  const aaa = () => {
+    router.push("/Product/new");
+  };
+
+  const detail = (event) => {
+    router.push(`/Product/detail/${event.target.id}`);
+    //   };
+
+    const toggle = async (event) => {
+      await toggleitem({
+        variables: { useditemId: event.target.id },
+        refetchQueries: [
+          {
+            query: FETCH_USED_ITEMS,
+          },
+        ],
+      });
+    };
+    console.log(best);
+    console.log(best?.fetchUseditemsOfTheBest[0].images[0]);
+  };
+
   const router = useRouter();
   const [keyword, setKeyword] = useState("");
-  const { data, refetch } = useQuery(FETCH_USED_ITEMS);
+  const { data: zzz, refetch } = useQuery(FETCH_USED_ITEMS);
 
   function onClickMoveToProductNew() {
     router.push("/product/new");
@@ -26,12 +174,16 @@ export default function ProductMain() {
       data={data}
       refetch={refetch}
       keyword={keyword}
-      // dataBoardsCount={dataBoardsCount}
-      // startPage={startPage}
-      // setStartPage={setStartPage}
       onClickMoveToProductNew={onClickMoveToProductNew}
       onClickMoveToProductDetail={onClickMoveToProductDetail}
       onChangeKeyword={onChangeKeyword}
+      item={item}
+      best={best}
+      baskets={baskets}
+      onClickBasket={onClickBasket}
+      aaa={aaa}
+      detail={detail}
+      // toggle={toggle}
     />
   );
 }
