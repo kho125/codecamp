6c41d898077895b41a1545a65daa1bfd48c4d08f
김경호 <kyeongho94@gmail.com>
2021-10-09 22:48:58 +0900

    삭제

--- freeboard_frontend/src/components/units/mypage222/charge/MyPageCharge.queries.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const CREATE_POINT_TRANSACTION_OF_LOADING = gql`
-  mutation createPointTransactionOfLoding($impUid: ID!) {
-    createPointTransactionOfLoading(impUid: $impUid) {
-      _id
-    }
-  }
-`;
